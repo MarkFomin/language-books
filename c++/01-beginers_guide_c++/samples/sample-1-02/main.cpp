6fb90485ec95ab639adbd07f7663b25479db1b8b
mark <fomin@infotek.ru>
2021-01-12 19:22:40 +0300

    add samples

---
+++ c++/01-beginers_guide_c++/samples/sample-1-02/main.cpp
@@ -0,0 +1,15 @@
+//Использование переменной.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int length; //здесь объявляется переменная
+	length = 7; //здесь length присваивается значение 7
+	cout << " Длинна равна ";
+	cout << length; //здесь выводится 7
+	cout << "\n";
+	
+	return 0;
+}
---
+++ c++/01-beginers_guide_c++/samples/sample-1-03/main.cpp
@@ -0,0 +1,19 @@
+//Использование оператора.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int length; //объявляение переменной
+	int width; //объявляение другой переменной
+	int area; //объявляение тертьей переменной
+	length = 7; //здесь length присваивается значение 7
+	width = 5; //здесь width присваивается значение 5
+	area = length*width; //вычисление площади
+	cout << " Площадь равна ";
+	cout << area; //здесь выводится 35
+	cout << "\n";
+	
+	return 0;
+}
