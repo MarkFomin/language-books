fa5597d13c14f32aaf6d2def27c14d5396fe8973
Mark Fomin <fomin@infotek.ru>
2022-02-25 23:04:39 +0300

    testserver.pro: add task 6

--- c++/skill_test/testserver.pro/task-6/main.cpp
+++ c++/skill_test/testserver.pro/task-6/main.cpp
@@ -12,7 +12,42 @@
  * 5) bool operator<(Student&);
  */
 #include<iostream>
+#include<string>
+#include<algorithm>
+#include<list>
 using namespace std;
+class Student {
+	string name;
+	string surname;
+public:
+	Student(){}
+	Student(const string &n, const string &s):name(n),surname(s){}
+	const string &getSurname(){return surname;}
+	const string &getName(){return name;}
+	void show(){
+		cout<<name<<" "<<surname<<endl;
+	}
+};
+class Finder {
+	string surname;
+public:
+	Finder(const string &s):surname(s){};
+	bool operator()(Student& stud){
+		return surname==stud.getSurname();
+	}
+};
 int main(){
+	list<Student> students;
+	students.push_back(Student("Tom", "Hanks"));
+	students.push_back(Student("Jone", "Loide"));
+	students.push_back(Student("Rone", "Forgart"));
+	
+	Finder finder("Hanks");
+	list<Student>::iterator it = find_if (students.begin(), students.end(), finder);
+	if(it!=students.end()){
+		it->show();
+	} else {
+		cout<<"Not found.\n";
+	}
 	return 0;
 }
